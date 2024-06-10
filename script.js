ead252ccf7662fdb8d20da245d0f8c2c7a350181
Glitch (glitch-hello-website) <none>
2024-06-10 12:50:39 +0000

    💈📼 Checkpoint ./script.js:28624378/56 ./index.html:28624378/453 ./style.css:28624378/36

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ window.htmx.defineExtension("alpine-morph", {
 });
 
 Alpine.data("board", () => ({
-  numbers: [0, 1, 2, 3, 4, 5],
+  numbers: [4,11,12,10,6,6,4,10,8,3,9,5,11,5,6,8,3,2],
 }));
 
 Alpine.start();
